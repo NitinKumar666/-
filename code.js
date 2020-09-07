0a7fd45b6d9cdd431bebf4324d88371e448ccf5c
NitinKumar666 <64644103+NitinKumar666@users.noreply.github.com>
2020-09-07 21:10:48 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,113 @@
+onEvent("letsgobutton", "click", function(event ) {
+  setScreen("optionScreen");
+});
+/*onEvent("button1", "click", function( ) {
+    getText("text_input1")== "Idly";
+    setScreen("idlyScreen");
+  });*/
+    
+  onEvent("button4", "click", function(event ) {
+  setScreen("choleScreen");
+});
+onEvent("button5", "click", function(event ) {
+  setScreen("idlyScreen");
+});
+onEvent("button2", "click", function(event ) {
+  setScreen("chooseScreen");
+});
+onEvent("button3", "click", function(event ) {
+  setScreen("pavScreen");
+});
+onEvent("button6", "click", function(event ) {
+  setScreen("dosaScreen");
+});
+onEvent("button9", "click", function(event ) {
+  setScreen("dosaQuantity");
+});
+onEvent("button17", "click", function(event ) {
+  setScreen("dosa2");
+});
+onEvent("button18", "click", function(event ) {
+  setScreen("dosa3");
+});
+onEvent("button19", "click", function(event ) {
+  setScreen("dosa4");
+});
+onEvent("button16", "click", function(event ) {
+  setScreen("dosa5");
+});
+onEvent("button20", "click", function(event ) {
+  setScreen("dosa6");
+});
+onEvent("button1", "click", function(event ) {
+  setScreen("idly2");
+});
+onEvent("button10", "click", function(event ) {
+  setScreen("idly3");
+});
+onEvent("button11", "click", function(event ) {
+  setScreen("idly4");
+});
+onEvent("button24", "click", function(event ) {
+  setScreen("idlyQuantity");
+});
+onEvent("button7", "click", function(event ) {
+  setScreen("cakeScreen");
+});
+onEvent("button14", "click", function(event ) {
+  setScreen("pav4");
+});
+onEvent("button15", "click", function(event ) {
+  setScreen("pav6");
+});
+onEvent("button22", "click", function(event ) {
+  setScreen("chooseScreen");
+});
+onEvent("button23", "click", function(event ) {
+  setScreen("choooseScreen");
+});
+onEvent("button35", "click", function(event ) {
+  setScreen("chole4");
+});
+onEvent("button25", "click", function(event ) {
+  setScreen("chooseScreen");
+});
+onEvent("button26", "click", function(event ) {
+  setScreen("chooseScreen");
+});
+onEvent("button28", "click", function(event ) {
+  setScreen("chooseScreen");
+});
+onEvent("button29", "click", function(event ) {
+  setScreen("chooseScreen");
+});
+onEvent("button27", "click", function(event ) {
+  setScreen("chooseScreen");
+});
+onEvent("button30", "click", function(event ) {
+  setScreen("chooseScreen");
+});
+onEvent("button31", "click", function(event ) {
+  setScreen("chooseScreen");
+});
+onEvent("button32", "click", function(event ) {
+  setScreen("chooseScreen");
+});
+onEvent("button33", "click", function(event ) {
+  setScreen("chooseScreen");
+});
+onEvent("button34", "click", function(event ) {
+  setScreen("chooseScreen");
+});
+onEvent("button36", "click", function(event ) {
+  setScreen("chooseScreen");
+});
+onEvent("button23", "click", function(event ) {
+  setScreen("cake4");
+});
+onEvent("button37", "click", function(event ) {
+  setScreen("chooseScreen");
+});
+onEvent("button13", "click", function(event ) {
+  setScreen("screen3");
+});
